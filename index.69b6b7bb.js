234c1d13dfe592e0899c235134f404a61851cbd2
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-06-19 14:45:14 +0300

    make build

---
+++ index.69b6b7bb.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("form .field input").forEach(function(e){var t=document.createElement("label");t.className="field-label",t.setAttribute("for",e.id),t.textContent=e.name,e.setAttribute("placeholder",e.name),e.parentNode.insertBefore(t,e)});
+//# sourceMappingURL=index.69b6b7bb.js.map
